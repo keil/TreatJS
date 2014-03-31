9a553182e34a65227e239d0da5c128c81aa1bba9
keilr <keilr@e3db86c2-31ce-0310-bca5-be3edd1b5589>
2014-03-31 07:42:04 +0000

    sync
    git-svn-id: https://proglang.informatik.uni-freiburg.de/svn/proglang/projects/jsproxy/branches/TreatJS@24050 e3db86c2-31ce-0310-bca5-be3edd1b5589

---
+++ src/treat.js
@@ -0,0 +1,44 @@
+/*
+ * TreatJS: Higher-Order Contracts for JavaScript 
+ * http://proglang.informatik.uni-freiburg.de/treatjs/
+ *
+ * Copyright (c) 2014, Proglang, University of Freiburg.
+ * http://proglang.informatik.uni-freiburg.de/treatjs/
+ * All rights reserved.
+ *
+ * Released under the MIT license
+ * http://proglang.informatik.uni-freiburg.de/treatjs/license
+ *
+ * Author Matthias Keil
+ * http://www.informatik.uni-freiburg.de/~keilr/
+ */
+(function(load, logger) {
+
+        // base source files
+        var base = ['treat.system.js','treat.base.js','treat.config.js'];
+        // core api
+        var core = ['core/treat.violation.js','core/treat.sandbox.js','core/treat.callback.js','core/treat.contract.js'];
+
+        function loadSource(files) {
+                if(load) for(var i=0; i<files.length; i++) {
+                        logger("@LOAD srv/" + files[i]);
+                        load('src/' + files[i]);
+                }
+        }
+
+       
+        loadSource(base);
+        loadSource(core);
+
+
+
+        
+
+
+})(load, print);
+
+
+
+// TODO:
+// debug package, mit prinbt, und log functionen
+
