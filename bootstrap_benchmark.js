32cc2c018109c8299f68f4702f37fbaaa55d6372
keilr <keilr@e3db86c2-31ce-0310-bca5-be3edd1b5589>
2014-04-03 11:16:30 +0000

    sync
    git-svn-id: https://proglang.informatik.uni-freiburg.de/svn/proglang/projects/jsproxy/branches/TreatJS@24125 e3db86c2-31ce-0310-bca5-be3edd1b5589

---
+++ bootstrap_benchmark.js
@@ -0,0 +1,31 @@
+/*
+ * TreatJS: Higher-Order Contracts for JavaScript 
+ * http://proglang.informatik.uni-freiburg.de/treatjs/
+ *
+ * Copyright (c) 2014, Proglang, University of Freiburg.
+ * http://proglang.informatik.uni-freiburg.de/treatjs/
+ * All rights reserved.
+ *
+ * Released under the MIT license
+ * http://proglang.informatik.uni-freiburg.de/treatjs/license
+ *
+ * Author Matthias Keil
+ * http://www.informatik.uni-freiburg.de/~keilr/
+ */
+load("src/treat.js");
+load("test/contracts.js");
+
+//  makes TreatJS available under $
+var $ = _ ;
+
+// verbose/ false
+$.Config.Verbose.sandbox = false;
+$.Config.Verbose.assert = false;
+
+$.Config.assertion = true;
+$.Config.membrane = true;
+$.Config.decompile = false;
+
+load("benchmark/octane/run.js")
+
+quit();
