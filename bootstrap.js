c0cf747536acadaf68cd3d0515d745f2c02174ee
keilr <keilr@e3db86c2-31ce-0310-bca5-be3edd1b5589>
2014-05-09 13:03:24 +0000

    sync
    git-svn-id: https://proglang.informatik.uni-freiburg.de/svn/proglang/projects/jsproxy/branches/TreatJS@24277 e3db86c2-31ce-0310-bca5-be3edd1b5589

--- bootstrap.js
+++ bootstrap.js
@@ -21,21 +21,36 @@ function run(file) {
   load(file);
 }
 
-/*
-   run("test/contract/basecontracts.js");
-   run("test/contract/functioncontracts.js");
+// contracts
+run("test/contract/basecontracts.js");
+run("test/contract/functioncontracts.js");
+run("test/contract/objectcontracts.js");
 
-   run("test/contract/objectcontracts.js");
+run("test/contract/andcontract.js");
+run("test/contract/orcontract.js");
+run("test/contract/notcontract.js");
 
-   run("test/contract/andcontract.js");
-   run("test/contract/orcontract.js");
-   run("test/contract/notcontract.js");
+run("test/contract/withcontract.js");
+run("test/contract/dependentcontracts.js");
+run("test/contract/constructor.js");
 
-   run("test/contract/withcontract.js");
-   run("test/contract/dependentcontracts.js");
+// sandbox tests
+run("test/sandbox/sandbox.js");
+run("test/sandbox/bind.js");
 
-   run("test/contract/constructor.js");
-   */
+// callback tests
+run("test/miscellaneous/logic.js");
 
+// behavior
+run("test/behavior/negative.js");
+run("test/behavior/positive.js");
+run("test/behavior/strict.js");
+run("test/behavior/weak.js");
+
+// convinience contarcts
+run("test/convenience/functioncontracts.js");
+run("test/convenience/objectcontracts.js.js");
+//run("test/convenience/dependentcontracts.js.js"); // not implemented
+//run("test/convenience/methodcontracts.js.js"); // not implemented
 
 quit();
