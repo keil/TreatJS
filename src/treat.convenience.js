d6c6360d9340950056e8fdb0c50b3b81a791e461
keilr <keilr@e3db86c2-31ce-0310-bca5-be3edd1b5589>
2014-05-12 08:29:09 +0000

    sync
    git-svn-id: https://proglang.informatik.uni-freiburg.de/svn/proglang/projects/jsproxy/branches/TreatJS@24283 e3db86c2-31ce-0310-bca5-be3edd1b5589

--- src/treat.convenience.js
+++ src/treat.convenience.js
@@ -42,6 +42,7 @@
   var RegExpMap = _.Map.RegExpMap;
 
   var Blank = new BaseContract(function(){return true;}, "-");
+  var BlankC = new ContractConstructor(function(){return Blank;});
 
   // TODO:
   // * complete testcases
@@ -107,6 +108,53 @@
   }
   SimpleFunctionContract.prototype = new FunctionContract(Blank, Blank);
 
+  // __  __     _   _            _  ___         _               _   
+  //|  \/  |___| |_| |_  ___  __| |/ __|___ _ _| |_ _ _ __ _ __| |_ 
+  //| |\/| / -_)  _| ' \/ _ \/ _` | (__/ _ \ ' \  _| '_/ _` / _|  _|
+  //|_|  |_\___|\__|_||_\___/\__,_|\___\___/_||_\__|_| \__,_\__|\__|
+
+  function AdvancedMethodContract(domain, range, context, strict) {
+    if(!(this instanceof AdvancedMethodContract)) return new AdvancedMethodContract(domain, range, context, strict);
+
+    if(!(range instanceof Contract)) error("Wrong Contract", (new Error()).fileName, (new Error()).lineNumber);
+    if(!(context instanceof Contract)) error("Wrong Contract", (new Error()).fileName, (new Error()).lineNumber);
+  
+    if(domain instanceof Array) {
+      MethodContract.call(this, AdvancedObjectContract(domain, strict), range, context);
+    } else if(domain instanceof Object) {
+      MethodContract.call(this, AdvancedObjectContract(domain, strict), range, context);
+    } else error("Wrong Contract", (new Error()).fileName, (new Error()).lineNumber);
+  }
+  AdvancedMethodContract.prototype = new MethodContract(Blank, Blank, Blank);
+
+  function SimpleMethodContract() {
+    if(!(this instanceof SimpleMethodContract)) {
+      var base = Object.create(SimpleMethodContract.prototype);
+      SimpleMethodContract.apply(base, arguments);
+      return base;
+    }
+    if(arguments.length < 3) error("Wrong Contract", (new Error()).fileName, (new Error()).lineNumber);
+
+    var domain = [];
+    for(var i=0; i<arguments.length;i++) domain[i]=arguments[i];
+
+    // TODO:
+    // a normal function contract is not strict
+    // make a seperate *strict* simple function conract
+    // var sign = domain.pop();
+    // var strict = domain.pop();is not really intended 
+    
+    var context = domain.pop();
+    var range = domain.pop();
+
+    MethodContract.call(this, AdvancedObjectContract(domain, false), range);
+  }
+  SimpleMethodContract.prototype = new MethodContract(Blank, Blank, Blank);
+
+
+
+
+
   // TODO:
   // * bring derivates
   // * Simple Dependen Contract
@@ -117,13 +165,31 @@
   //| |) / -_) '_ \/ -_) ' \/ _` / -_) ' \  _| (__/ _ \ ' \  _| '_/ _` / _|  _|
   //|___/\___| .__/\___|_||_\__,_\___|_||_\__|\___\___/_||_\__|_| \__,_\__|\__|
   //         |_|                                                               
- 
+
+    function SimpleDependentContract(name, contract) {
+      if(!(this instanceof SimpleDependentContract)) return new SimpleDependentContract(name, contract);
+
+      function domain(domainArgs) {
+        var binding = {};
+        binding[name]=domainArgs;
+        return _.With(binding, contract);
+      } 
+      DependentContract.call(this, _.Constructor(domain));
+  }
+  SimpleDependentContract.prototype = new DependentContract(BlankC);
+
   /**
    * Convenience Contracts
    */
 
+  __define("AdvancedObjectContract", AdvancedObjectContract, _);
+
   __define("AdvancedFunctionContract", AdvancedFunctionContract, _);
   __define("SimpleFunctionContract", SimpleFunctionContract, _);
-  __define("AdvancedObjectContract", AdvancedObjectContract, _);
+
+  __define("AdvancedMethodContract", AdvancedMethodContract, _);
+  __define("SimpleMethodContract", SimpleMethodContract, _);
+
+  __define("SimpleDependentContract", SimpleDependentContract, _);
 
 })(_);
