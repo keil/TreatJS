1f22ce9c6b2177a4d36f40a5b6e4635536eedabd
keilr <keilr@e3db86c2-31ce-0310-bca5-be3edd1b5589>
2014-06-04 15:20:22 +0000

    add type information
    git-svn-id: https://proglang.informatik.uni-freiburg.de/svn/proglang/projects/jsproxy/branches/TreatJS@24489 e3db86c2-31ce-0310-bca5-be3edd1b5589

---
+++ benchmark/typedoctane/TYPES.js
@@ -0,0 +1,21 @@
+//-- BEGIN: TYPES --
+
+_TYPES_=[];
+
+_TYPES_['richards.js#1']=[];
+_TYPES_['richards.js#1'][0]='number';
+_TYPES_['richards.js#1'][1]='number';
+_TYPES_['richards.js#1'][2]='object';
+_TYPES_['richards.js#1'][3]='number';
+_TYPES_['richards.js#1'][-1]='undefined';
+_TYPES_['richards.js#2']=[];
+_TYPES_['richards.js#2'][0]='number';
+_TYPES_['richards.js#2'][1]='number';
+_TYPES_['richards.js#2'][2]='object';
+_TYPES_['richards.js#2'][-1]='undefined';
+_TYPES_['richards.js#3']=[];
+_TYPES_['richards.js#3'][0]='number';
+_TYPES_['richards.js#3'][1]='number';
+_TYPES_['richards.js#3'][2]='object';
+_TYPES_['richards.js#3'][-1]='undefined';
+//-- END: TYPES --
