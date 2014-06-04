e3e5c7bb264ae6d5d519397ca30d3d805695c2ed
keilr <keilr@e3db86c2-31ce-0310-bca5-be3edd1b5589>
2014-06-04 12:57:02 +0000

    sync
    git-svn-id: https://proglang.informatik.uni-freiburg.de/svn/proglang/projects/jsproxy/branches/TreatJS@24478 e3db86c2-31ce-0310-bca5-be3edd1b5589

--- bootstrap_typedoctane.js
+++ bootstrap_typedoctane.js
@@ -77,25 +77,90 @@ function _freshID_() {
   return ("@"+_counter_);
 }
 
-//function _wrap_ (f) { return f; }
+function _wrap_ (f) { return f; }
 
-function _wrap_ (f) {
+function _wrap_X (f) {
   var fid = _freshID_();
   _types_[fid] = [];
   return new Proxy(f, new _TypeHandler_(fid));
 }
 
 
-
-
 load("benchmark/typedoctane/run.js")
+
+
+var IsNumber = _.BaseContract(function(arg) {
+  return ((typeof arg) === "number");
+},"IsNumber");
+
+var IsString = _.BaseContract(function(arg) {
+  return ((typeof arg) === "string");
+},"IsString");
+
+var IsBoolean = _.BaseContract(function(arg) {
+  return ((typeof arg) === "boolean");
+},"IsBoolean");
+
+var IsUndef = _.BaseContract(function(arg) {
+  return (arg === undefined);
+},"IsUndef");
+
+var Any = _.BaseContract(function(arg) {
+  return true; 
+},"Any");
+
+var IsObject =  _.With({Object:Object}, _.BaseContract(function(arg) {
+  return (arg instanceof Object); 
+},"InstanceOfObject"));
+
+var IsFunction =  _.With({Function:Function}, _.BaseContract(function(arg) {
+  return (arg instanceof Function); 
+},"InstanceOfFunction"));
+
+var IsArray = _.With({Array:Array}, _.BaseContract(function(arg) {
+  return (arg instanceof Array);
+},"IsArray"));
+
+// TODO
+var _TYPES_ = _types_;
+
+function _makeTypeContract_ (t) {
+  if(t == "number") return IsNumber;
+  else if(t == "string") return IsString;
+  else if(t == "boolean") return IsBoolean;
+  else if(t == "undefined") return IsUndef;
+  else if(t == "object") return IsObject;
+  else if(t == "function") return IsFunction;
+  else if(t == "array") return IsArray;
+  else return Any;
+}
+
+function _makeContract_(fid) {
+  var types = _TYPES_[fid];
+
+  var args = [];
+  for(var i in types) {
+    if(i!=-1) {
+      args[i] = _makeTypeContract_(_TYPES_[fid][i]);
+    }
+  }
+
+  var map = _.Map.StringMap(args);
+  var domain = _.ObjectContract(map);
+  var range = _makeTypeContract_(_TYPES_[fid][-1]);
+  var contract = _.FunctionContract(domain, range);
+  return contract;
+}
 
 print("--");
 for(fid in _types_) {
   for(i in _types_[fid]) {
     print("_TYPES_["+fid+"]["+i+"]="+_types_[fid][i]+";");
   }
+  print(_makeContract_(fid));
+  print("");
 }
 print("--");
+
 
 quit();
