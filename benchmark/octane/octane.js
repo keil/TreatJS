26afdd5def1c1991e153704ac2e55978c742ceff
keilr <keilr@e3db86c2-31ce-0310-bca5-be3edd1b5589>
2014-06-20 14:22:04 +0000

    sync
    git-svn-id: https://proglang.informatik.uni-freiburg.de/svn/proglang/projects/jsproxy/branches/TreatJS@24572 e3db86c2-31ce-0310-bca5-be3edd1b5589

---
+++ benchmark/octane/octane.js
@@ -0,0 +1,22 @@
+/*
+ * TreatJS: Higher-Order Contracts for JavaScript 
+ * http://proglang.informatik.uni-freiburg.de/treatjs/
+ *
+ * Copyright (c) 2014, Proglang, University of Freiburg.
+ * http://proglang.informatik.uni-freiburg.de/treatjs/
+ * All rights reserved.
+ *
+ * Released under the MIT license
+ * http://proglang.informatik.uni-freiburg.de/treatjs/license
+ *
+ * Author Matthias Keil
+ * http://www.informatik.uni-freiburg.de/~keilr/
+ */
+
+//  makes TreatJS available under $
+//  Note: required to run the testcases
+var $ = _ ;
+
+load("benchmark/octane/run.js")
+
+quit();
