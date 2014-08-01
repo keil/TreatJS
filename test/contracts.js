e09d763e3ae075af59490293c7976ba8c4cd146a
keilr <keilr@e3db86c2-31ce-0310-bca5-be3edd1b5589>
2014-08-01 11:55:51 +0000

    add new contract
    git-svn-id: https://proglang.informatik.uni-freiburg.de/svn/proglang/projects/jsproxy/branches/TreatJS@24646 e3db86c2-31ce-0310-bca5-be3edd1b5589

--- test/contracts.js
+++ test/contracts.js
@@ -100,143 +100,32 @@ var instanceOfError = TreatJS.With({Error:Error}, TreatJS.BaseContract(function(
 
 var isNaN= TreatJS.BaseContract(function(arg) {
   return (arg === NaN);
-},"IsNaN");
+},"isNaN");
 
 var isUndefined = TreatJS.BaseContract(function(arg) {
   return (arg === undefined);
-},"IsUndef");
+},"isUndefined");
 
 var isNull = TreatJS.BaseContract(function(arg) {
   return (arg === "boolean");
-},"IsNull");
+},"isNull");
 
 var isTrue = TreatJS.BaseContract(function(arg) {
   return (arg) ? true : false; 
-},"IsTrue");
+},"isTrue");
 
 var isFalse = TreatJS.BaseContract(function(arg) {
   return (arg) ? false : true; 
-},"IsFalse");
+},"isFalse");
 
 
 
+/* Is-Contracts */
 
+var isPrimitiveValue = TreatJS.BaseContract(function(arg) {
+  return (target !== Object(target)) ? true : false; 
+},"isPrimitiveValue");
 
-
-/*
-
-
-
-   var _ = TreatJS;
-
-   var IsNumber = _.BaseContract(function(arg) {
-   return ((typeof arg) === "number");
-   },"IsNumber");
-
-   var IsString = _.BaseContract(function(arg) {
-   return ((typeof arg) === "string");
-   },"IsString");
-
-   var IsBoolean = _.BaseContract(function(arg) {
-   return ((typeof arg) === "boolean");
-   },"IsBoolean");
-
-
-
-   var IsNaN= _.BaseContract(function(arg) {
-   return (arg === NaN);
-   },"IsNaN");
-
-   var IsUndef = _.BaseContract(function(arg) {
-   return (arg === undefined);
-   },"IsUndef");
-
-   var IsNull = _.BaseContract(function(arg) {
-   return (arg === "boolean");
-   },"IsNull");
-
-
-
-
-   var Any = _.BaseContract(function(arg) {
-   return true; 
-   },"Any");
-
-   var True = _.BaseContract(function(arg) {
-   return (arg) ? true : false; 
-   },"True");
-
-   var False = _.BaseContract(function(arg) {
-   return (arg) ? false : true; 
-   },"False");
-
-
-
-
-   var Isbject =  _.BaseContract(function(arg) {
-   return ((typeof arg) === "object");
-   },"IsObject");
-
-   var IsFunction = _.BaseContract(function(arg) {
-   return ((typeof arg) === "function");
-   },"IsFunction");
-
-//var IsArray = _.With({Array:Array}, _.BaseContract(function(arg) {
-//  return ((typeof arg) === "number");
-//},"IsArray"));
-
-/*
-
-var Isbject =  _.With({Object:Object}, _.BaseContract(function(arg) {
-return (arg instanceof Object); 
-},"InstanceOfObject"));
-
-var IsFunction =  _.With({Function:Function}, _.BaseContract(function(arg) {
-return (arg instanceof Function); 
-},"InstanceOfFunction"));
-
-var IsArray = _.With({Array:Array}, _.BaseContract(function(arg) {
-return (arg instanceof Array);
-},"IsArray"));
-*/
-
-
-/*
-
-   var ContainsToString = _.BaseContract(function(obj) {
-   return (obj.hasOwnProperty("toString")) ? true : false;
-   },"ContainsToString");
-
-
-   var GreaterThanZero = _.BaseContract(function(arg) {
-   return (arg>0);
-   },"GreaterThanZero");
-
-
-   var AbsLowerThan100 = _.BaseContract(function (val) {
-   return (Math.abs(val) < 100)
-   },"AbsLowerThan100");
-
-
-
-   var InstanceOfTarget =  _.BaseContract(function(arg) {
-   return (arg instanceof target); 
-   },"InstanceOfTarget");
-
-
-
-   var InstanceOfObject =  _.With({Object:Object}, _.BaseContract(function(arg) {
-   return (arg instanceof Object); 
-   },"InstanceOfObject"));
-
-   var InstanceOfFunction =  _.With({Function:Function}, _.BaseContract(function(arg) {
-   return (arg instanceof Function); 
-   },"InstanceOfFunction"));
-
-   var InstanceOfArray = _.With({Array:Array}, _.BaseContract(function(arg) {
-   return (arg instanceof Array);
-   },"InstanceOfArray"));
-
-   var IsArray = InstanceOfArray;
-
-*/
+var isNativeFunction = TreatJS.BaseContract(function(arg) {
+  return (Function.prototype.toString.apply(func).indexOf('[native code]') > -1); 
+},"isNativeFunction");
