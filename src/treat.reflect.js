bb4173fc046ef9b8cebf4e7a22d5be84ab3728e9
keilr <keilr@e3db86c2-31ce-0310-bca5-be3edd1b5589>
2015-01-15 13:46:55 +0000

    sync
    git-svn-id: https://proglang.informatik.uni-freiburg.de/svn/proglang/projects/jsproxy/branches/TreatJS@24911 e3db86c2-31ce-0310-bca5-be3edd1b5589

--- src/treat.reflect.js
+++ src/treat.reflect.js
@@ -37,243 +37,42 @@
 
 
 
-  function ReflectionContract() { // TODO, name and trap function, which is later defined ?
-    if(!(this instanceof ReflectionContract)) return new ReflectionContract(map);
+  function ReflectionContract(trap, sub) {
+    if(!(this instanceof ReflectionContract)) return new ReflectionContract(trap, sub);
 
-    // if(!(map instanceof Map)) error("Wrong Contract", (new Error()).fileName, (new Error()).lineNumber); 
+    if((typeof trap)!=="string") error("Wrong Contract", (new Error()).fileName, (new Error()).lineNumber); 
+    if(!(sub instanceof Contract)) error("Wrong Contract", (new Error()).fileName, (new Error()).lineNumber);
 
-    /*Object.defineProperties(this, {
-      "strict": {
-      get: function () { return map.strict; } },
-      "map": {
-      get: function () { return map; } }
-      });*/
+    Object.defineProperties(this, {
+      "trap": {
+        get: function () { return trap; } },
+      "sub": {
+        get: function () { return sub; } }
+    });
 
-    /*this.toString = function() {
-      var lbr = (map.strict) ? "{" : "|";
-      var rbr = (map.strict) ? "}" : "|";
-      return lbr + map.toString() + rbr;
-      };*/
+    this.toString = function() {
+      return "" +  trap + " @ " + sub.toString();
+    };
   }
   ReflectionContract.prototype = new ImmediateContract();
 
 
 
-  function GetContract(target, name, receiver) {
-    if(!(this instanceof GetContract)) return new GetContract(target, name, receiver);
 
-    if(!(target instanceof Contract)) error("Wrong Contract", (new Error()).fileName, (new Error()).lineNumber);
-    if(!(name instanceof Contract)) error("Wrong Contract", (new Error()).fileName, (new Error()).lineNumber);
-    if(!(receiver instanceof Contract)) error("Wrong Contract", (new Error()).fileName, (new Error()).lineNumber);
-
-    Object.defineProperties(this, {
-      "target": {
-        get: function () { return target; } },
-      "name": {
-        get: function () { return name; } },
-      "receiver": {
-        get: function () { return receiver; } }
-    });
+  function GetContract(contract) {
+    if(!(this instanceof GetContract)) return new GetContract(contract);
+    ReflectionContract.call(this, "get", contract);
   }
-  GetContract.prototype = new ReflectionContract(); /// TODO
+  GetContract.prototype = Object.create(ReflectionContract.prototype);
 
-  function SetContract(target, name, value, receiver) {
-    if(!(this instanceof SetContract)) return new SetContract(target, name, value, receiver);
 
-    if(!(target instanceof Contract)) error("Wrong Contract", (new Error()).fileName, (new Error()).lineNumber);
-    if(!(name instanceof Contract)) error("Wrong Contract", (new Error()).fileName, (new Error()).lineNumber);
-    if(!(value instanceof Contract)) error("Wrong Contract", (new Error()).fileName, (new Error()).lineNumber);
-    if(!(receiver instanceof Contract)) error("Wrong Contract", (new Error()).fileName, (new Error()).lineNumber);
-
-    Object.defineProperties(this, {
-      "target": {
-        get: function () { return target; } },
-      "name": {
-        get: function () { return name; } },
-      "value": {
-        get: function () { return value; } },
-      "receiver": {
-        get: function () { return receiver; } }
-    });
+  function SetContract(contract) {
+    if(!(this instanceof SetContract)) return new SetContract(contract);
+    ReflectionContract.call(this, "set", contract);
   }
-  SetContract.prototype = new ReflectionContract(); /// TODO
+  SetContract.prototype = Object.create(ReflectionContract.prototype);
 
 
-
-
-
-  function assertWithRefelction(arg, contract, global, callbackHandler) {
-    // TODO, build refelction contract
-
-    if(contract instanceof GetContract) {
-      if(!(arg instanceof Object)) callbackHandler(Handle(_.Logic.True, _.Logic.False, _.Logic.False));
-
-      var handler = new GetHandler(contract, global, callbackHandler);
-      var proxy = new Proxy(arg, handler);
-      return proxy;
-    }
-
-    if(contract instanceof SetContract) {
-      if(!(arg instanceof Object)) callbackHandler(Handle(_.Logic.True, _.Logic.False, _.Logic.False));
-
-      print("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
-
-
-      var handler = new SetHandler(contract, global, callbackHandler);
-      var proxy = new Proxy(arg, handler);
-      return proxy;
-    }
-
-  }
-
-
-
-
-  function GetHandler(contract, global, handler) {
-    if(!(this instanceof GetHandler)) return new GetHandler(contract, global, handler);
-
-    var callback = GetCallback(handler, contract);
-
-    this.get = function(target, name, receiver) {
-
-      var targetC = _.assertWith(target, contract.target, global, callback.receiverHandler);
-      var nameC = _.assertWith(name, contract.name, global, callback.nameHandler);
-      var receiverC = _.assertWith(receiver, contract.receiver, global, callback.receiverHandler);
-
-      // TODO, possible to contarct the return directly
-      return targetC[nameC];
-    };
-  }
-
-  function SetHandler(contract, global, handler) {
-    if(!(this instanceof SetHandler)) return new SetHandler(contract, global, handler);
-
-    var callback = SetCallback(handler, contract);
-
-    this.set = function(target, name, value, receiver) {
-      var targetC = _.assertWith(target, contract.target, global, callback.targetHandler);
-      var nameC = _.assertWith(name, contract.name, global, callback.nameHandler);
-      var valueC = _.assertWith(value, contract.value, global, callback.valueHandler);
-      var receiverC = _.assertWith(receiver, contract.receiver, global, callback.receiverHandler);
-
-      // TODO, possible to contarct the return directly
-      return (targetC[nameC]=valueC);
-    };
-  }
-
-
-  var and = _.Logic.and;
-
-  function GetCallback(handler, contract) {
-    if(!(this instanceof GetCallback)) return new GetCallback(handler, contract);
-    else Callback.apply(this, arguments);
-
-    var target =  Handle.new();
-    var name =  Handle.new();
-    var receiver = Handle.new();
-
-    __getter("caller", function() {
-      return and(target.caller, and(name.caller, receiver.caller));
-    }, this);
-
-    __getter("callee", function() {
-      return and(target.callee, and(name.callee, receiver.callee));
-    }, this);
-
-    __getter("contract", function() {
-      return and(this.caller, this.callee);
-    }, this);
-
-    __define("blame", function() {
-      return contract;
-    }, this);
-
-    __getter("targetHandler", function() {
-      return (function(handle) {
-        target = Handle.update(target, handle);
-        handler(this);    
-      }).bind(this);
-    }, this);
-
-    __getter("nameHandler", function() {
-      return (function(handle) {
-        name = Handle.update(name, handle);
-        handler(this);
-      }).bind(this);
-    }, this);
-
-    __getter("receiverHandler", function() {
-      return (function(handle) {
-        receiver = Handle.update(receiver, handle);
-        handler(this);
-      }).bind(this);
-    }, this);
-
-  }
-  GetCallback.prototype = Callback.prototype;
-  GetCallback.prototype.toString = function() {
-    return "<GetCallback>";
-  }
-
-
-
-  function SetCallback(handler, contract) {
-    if(!(this instanceof SetCallback)) return new SetCallback(handler, contract);
-    else Callback.apply(this, arguments);
-
-    var target =  Handle.new();
-    var name =  Handle.new();
-    var value =  Handle.new();
-    var receiver = Handle.new();
-
-    __getter("caller", function() {
-      return and(and(target.caller, name.caller), and(value.caller, receiver.caller));
-    }, this);
-
-    __getter("callee", function() {
-      return and(and(target.callee, name.callee), and(value.callee, receiver.callee));
-    }, this);
-
-    __getter("contract", function() {
-      return and(this.caller, this.callee);
-    }, this);
-
-    __define("blame", function() {
-      return contract;
-    }, this);
-
-    __getter("targetHandler", function() {
-      return (function(handle) {
-        target = Handle.update(target, handle);
-        handler(this);    
-      }).bind(this);
-    }, this);
-
-    __getter("nameHandler", function() {
-      return (function(handle) {
-        name = Handle.update(name, handle);
-        handler(this);
-      }).bind(this);
-    }, this);
-
-    __getter("valueHandler", function() {
-      return (function(handle) {
-        value = Handle.update(value, handle);
-        handler(this);
-      }).bind(this);
-    }, this);
-
-    __getter("receiverHandler", function() {
-      return (function(handle) {
-        receiver = Handle.update(receiver, handle);
-        handler(this);
-      }).bind(this);
-    }, this);
-  }
-  SetCallback.prototype = Callback.prototype;
-  SetCallback.prototype.toString = function() {
-    return "<SetCallback>";
-  }
 
 
 
@@ -282,21 +81,121 @@
 
 
   /*
-     function ReflectionHandler(contract, global, handler) {
-     if(!(this instanceof ReflectionHandler)) return new ReflectionHandler(contract, global, handler);
 
-     var noopHandler = {
-     get: function(target, name, receiver) {
-     return target[name];
-     },
-     set: function(target, name, value, receiver) {
-     return (target[name]=value);
+     function GetContract(target, name, receiver) {
+     if(!(this instanceof GetContract)) return new GetContract(target, name, receiver);
+
+     if(!(target instanceof Contract)) error("Wrong Contract", (new Error()).fileName, (new Error()).lineNumber);
+     if(!(name instanceof Contract)) error("Wrong Contract", (new Error()).fileName, (new Error()).lineNumber);
+     if(!(receiver instanceof Contract)) error("Wrong Contract", (new Error()).fileName, (new Error()).lineNumber);
+
+     Object.defineProperties(this, {
+     "target": {
+     get: function () { return target; } },
+     "name": {
+     get: function () { return name; } },
+     "receiver": {
+     get: function () { return receiver; } }
+     });
      }
-     };
+     GetContract.prototype = new ReflectionContract(); /// TODO
+
+     function SetContract(target, name, value, receiver) {
+     if(!(this instanceof SetContract)) return new SetContract(target, name, value, receiver);
+
+     if(!(target instanceof Contract)) error("Wrong Contract", (new Error()).fileName, (new Error()).lineNumber);
+     if(!(name instanceof Contract)) error("Wrong Contract", (new Error()).fileName, (new Error()).lineNumber);
+     if(!(value instanceof Contract)) error("Wrong Contract", (new Error()).fileName, (new Error()).lineNumber);
+     if(!(receiver instanceof Contract)) error("Wrong Contract", (new Error()).fileName, (new Error()).lineNumber);
+
+     Object.defineProperties(this, {
+     "target": {
+     get: function () { return target; } },
+     "name": {
+     get: function () { return name; } },
+     "value": {
+     get: function () { return value; } },
+     "receiver": {
+     get: function () { return receiver; } }
+     });
+     }
+     SetContract.prototype = new ReflectionContract(); /// TODO
+
+
+*/
 
 
 
-     return new Proxy(noopHandler, this);
+
+  function ReflectionHandler(contract, global, handler) {
+    if(!(this instanceof ReflectionHandler)) return new ReflectionHandler(contract, global, handler);
+
+    this.get = function(target, name, receiver) {
+      
+      if(name === contract.trap) {
+        return _.assertWith(target[name], contract.sub, global, handler);
+      } else {
+        return target[name];
+      }
+    };
+  }
+
+  function NoOpHandler() {
+    if(!(this instanceof NoOpHandler)) return new NoOpHandler();
+
+    // default get trap
+    this.get = function(target, name, receiver) {
+      return target[name];
+    };
+
+    // default set trap
+    this.set = function(target, name, value, receiver) {
+      return target[name]=value;
+    };
+  }
+
+
+  function assertWithRefelction(arg, contract, global, callbackHandler) {
+
+
+    if(contract instanceof ReflectionContract) {
+      if(!(arg instanceof Object)) callbackHandler(Handle(_.Logic.True, _.Logic.False, _.Logic.False));
+
+      var reflect = new ReflectionHandler(contract, global, callbackHandler);
+      var noop = new NoOpHandler();
+      var proxy = new Proxy(arg, new Proxy(noop, reflect));
+      return proxy;
+    }
+
+  }
+
+  /*
+     if(contract instanceof GetContract) {
+     if(!(arg instanceof Object)) callbackHandler(Handle(_.Logic.True, _.Logic.False, _.Logic.False));
+
+     var handler = new GetHandler(contract, global, callbackHandler);
+     var proxy = new Proxy(arg, handler);
+     return proxy;
+     }
+
+     if(contract instanceof SetContract) {
+     if(!(arg instanceof Object)) callbackHandler(Handle(_.Logic.True, _.Logic.False, _.Logic.False));
+
+     print("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
+
+
+     var handler = new SetHandler(contract, global, callbackHandler);
+     var proxy = new Proxy(arg, handler);
+     return proxy;
+     }
+
+
+     if(contract instanceof ReflectionContract) {
+     if(!(arg instanceof Object)) callbackHandler(Handle(_.Logic.True, _.Logic.False, _.Logic.False));
+
+     var handler = new GetHandler(contract, global, callbackHandler);
+     var proxy = new Proxy(arg, handler);
+     return proxy;
      }
      */
 
@@ -306,53 +205,234 @@
 
 
 
+  /*
+     function GetHandler(contract, global, handler) {
+     if(!(this instanceof GetHandler)) return new GetHandler(contract, global, handler);
+
+     var callback = GetCallback(handler, contract);
+
+     this.get = function(target, name, receiver) {
+
+     var targetC = _.assertWith(target, contract.target, global, callback.receiverHandler);
+     var nameC = _.assertWith(name, contract.name, global, callback.nameHandler);
+     var receiverC = _.assertWith(receiver, contract.receiver, global, callback.receiverHandler);
+
+  // TODO, possible to contarct the return directly
+  return targetC[nameC];
+  };
+  }
+
+  function SetHandler(contract, global, handler) {
+  if(!(this instanceof SetHandler)) return new SetHandler(contract, global, handler);
+
+  var callback = SetCallback(handler, contract);
+
+  this.set = function(target, name, value, receiver) {
+  var targetC = _.assertWith(target, contract.target, global, callback.targetHandler);
+  var nameC = _.assertWith(name, contract.name, global, callback.nameHandler);
+  var valueC = _.assertWith(value, contract.value, global, callback.valueHandler);
+  var receiverC = _.assertWith(receiver, contract.receiver, global, callback.receiverHandler);
+
+  // TODO, possible to contarct the return directly
+  return (targetC[nameC]=valueC);
+  };
+  }
+
+
+  var and = _.Logic.and;
+
+*/
+
+  /*
+     function GetCallback(handler, contract) {
+     if(!(this instanceof GetCallback)) return new GetCallback(handler, contract);
+     else Callback.apply(this, arguments);
+
+     var target =  Handle.new();
+     var name =  Handle.new();
+     var receiver = Handle.new();
+
+     __getter("caller", function() {
+     return and(target.caller, and(name.caller, receiver.caller));
+     }, this);
+
+     __getter("callee", function() {
+     return and(target.callee, and(name.callee, receiver.callee));
+     }, this);
+
+     __getter("contract", function() {
+     return and(this.caller, this.callee);
+     }, this);
+
+     __define("blame", function() {
+     return contract;
+     }, this);
+
+     __getter("targetHandler", function() {
+     return (function(handle) {
+     target = Handle.update(target, handle);
+     handler(this);    
+     }).bind(this);
+     }, this);
+
+     __getter("nameHandler", function() {
+     return (function(handle) {
+     name = Handle.update(name, handle);
+     handler(this);
+     }).bind(this);
+     }, this);
+
+     __getter("receiverHandler", function() {
+     return (function(handle) {
+     receiver = Handle.update(receiver, handle);
+     handler(this);
+     }).bind(this);
+     }, this);
+
+     }
+     GetCallback.prototype = Callback.prototype;
+     GetCallback.prototype.toString = function() {
+     return "<GetCallback>";
+     }
 
 
 
+     function SetCallback(handler, contract) {
+     if(!(this instanceof SetCallback)) return new SetCallback(handler, contract);
+     else Callback.apply(this, arguments);
+
+     var target =  Handle.new();
+     var name =  Handle.new();
+     var value =  Handle.new();
+     var receiver = Handle.new();
+
+     __getter("caller", function() {
+     return and(and(target.caller, name.caller), and(value.caller, receiver.caller));
+     }, this);
+
+     __getter("callee", function() {
+     return and(and(target.callee, name.callee), and(value.callee, receiver.callee));
+     }, this);
+
+__getter("contract", function() {
+  return and(this.caller, this.callee);
+}, this);
+
+__define("blame", function() {
+  return contract;
+}, this);
+
+__getter("targetHandler", function() {
+  return (function(handle) {
+    target = Handle.update(target, handle);
+    handler(this);    
+  }).bind(this);
+}, this);
+
+__getter("nameHandler", function() {
+  return (function(handle) {
+    name = Handle.update(name, handle);
+    handler(this);
+  }).bind(this);
+}, this);
+
+__getter("valueHandler", function() {
+  return (function(handle) {
+    value = Handle.update(value, handle);
+    handler(this);
+  }).bind(this);
+}, this);
+
+__getter("receiverHandler", function() {
+  return (function(handle) {
+    receiver = Handle.update(receiver, handle);
+    handler(this);
+  }).bind(this);
+}, this);
+}
+SetCallback.prototype = Callback.prototype;
+SetCallback.prototype.toString = function() {
+  return "<SetCallback>";
+}
 
 
 
+*/
 
 
 
+/*
+   function ReflectionHandler(contract, global, handler) {
+   if(!(this instanceof ReflectionHandler)) return new ReflectionHandler(contract, global, handler);
+
+   var noopHandler = {
+   get: function(target, name, receiver) {
+   return target[name];
+   },
+   set: function(target, name, value, receiver) {
+   return (target[name]=value);
+   }
+   };
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  // TODO
-
-  /**
-   * Reflection Contracts
+   return new Proxy(noopHandler, this);
+   }
    */
 
-  __define("Reflection", ReflectionContract, _);
 
-  __define("GetContract", GetContract, _);
-  __define("SetContract", SetContract, _);
 
-  __define("assertReflection", assertWithRefelction, _);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// TODO
+
+/**
+ * Reflection Contracts
+ */
+
+__define("Reflection", ReflectionContract, _);
+
+__define("GetContract", GetContract, _);
+__define("SetContract", SetContract, _);
+
+__define("assertReflection", assertWithRefelction, _);
 
 
 })(TreatJS);
