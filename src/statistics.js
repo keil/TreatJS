0b4f979a7d36e2206d51eb4338cb6a9fc62f0569
keilr <keilr@e3db86c2-31ce-0310-bca5-be3edd1b5589>
2015-01-15 08:49:15 +0000

    sznc
    git-svn-id: https://proglang.informatik.uni-freiburg.de/svn/proglang/projects/jsproxy/branches/TreatJS@24904 e3db86c2-31ce-0310-bca5-be3edd1b5589

--- src/statistics.js
+++ src/statistics.js
@@ -12,43 +12,47 @@
  * Author Matthias Keil
  * http://www.informatik.uni-freiburg.de/~keilr/
  */
+(function(_) {
 
+  function Statistic() {
+    if(!(this instanceof Statistic)) return new Statistic();
 
-function TreatJSStatistics() {
-  if(!(this instanceof TreatJSStatistics)) return new TreatJSStatistics();
+    var counts = new Array();
 
-  var counts = new Array();
-
-  this.inc = function(key) {
-    return (counts[key]) ? (counts[key]=counts[key]+1) : counts[key]=1;
-  }
-
-  this.reset = function() {
-    counts = new Array();
-  }
-
-  this.get = function(key) {
-    return counts[key];
-  }
-
-  this.print = function () {
-
-    if(_.Config.Verbose.statistic) {
-      __out(padding_right("TreatJS Statistics ", ".", 30));
-      __blank();
+    this.inc = function(key) {
+      return (counts[key]) ? (counts[key]=counts[key]+1) : counts[key]=1;
     }
 
-    function log(msg, value) {
+    this.reset = function() {
+      counts = new Array();
+    }
+
+    this.get = function(key) {
+      return counts[key];
+    }
+
+    this.print = function () {
+
       if(_.Config.Verbose.statistic) {
-        __subout(padding_right(msg + " ", ".", 30) + padding_left(value + "", " ", 9));
+        __out(padding_right("TreatJS Statistics ", ".", 30));
         __blank();
       }
+
+      function log(msg, value) {
+        if(_.Config.Verbose.statistic) {
+          __subout(padding_right(msg + " ", ".", 30) + padding_left(value + "", " ", 9));
+          __blank();
+        }
+      }
+
+      log("#" + this.ASSERT + " = ", counts[this.ASSERT]);
+      log("#" + this.BASE + " = ", counts[this.BASE]);
     }
-
-    log("#" + this.ASSERT + " = ", counts[this.ASSERT]);
-    log("#" + this.BASE + " = ", counts[this.BASE]);
   }
-}
 
-TreatJSStatistics.prototype.ASSERT = "ASSERT";
-TreatJSStatistics.prototype.BASE = "BASE";
+  Statistic.prototype.ASSERT = "ASSERT";
+  Statistic.prototype.BASE = "BASE";
+
+  __define("Statistic", Statistic, _);
+
+})(TreatJS);
