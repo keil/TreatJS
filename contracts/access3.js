0026f62ccfd145e98d450fa071a9faef1e24ed54
keilr <keilr@e3db86c2-31ce-0310-bca5-be3edd1b5589>
2015-01-26 14:39:21 +0000

    sync
    git-svn-id: https://proglang.informatik.uni-freiburg.de/svn/proglang/projects/jsproxy/branches/TreatJS@24960 e3db86c2-31ce-0310-bca5-be3edd1b5589

--- contracts/access3.js
+++ contracts/access3.js
@@ -8,7 +8,7 @@ load("lib/jscontest2/contract.js");
 load("lib/jscontest2/parser.js");
 
 
-var AccessContract = Contract.Constructor (function ctor (apcon) {
+var AccessContract = Contract.Constructor (function ctor (apcon, APC) {
   var any = Contract.Base(function(property) {
     return true;
   });
@@ -18,22 +18,32 @@ var AccessContract = Contract.Constructor (function ctor (apcon) {
   var writeable = Contract.Base(function(name) {
     return apcon.isWriteable(name);
   });
-  
+
   var AccessContract = Contract.Constructor(ctor);
   var Access = AccessContract.ctor
 
-//var Access = Contract.Constructor(ctor).ctor;
+  //var Access = Contract.Constructor(ctor).ctor;
 
-var membrane = Contract.Constructor(function membrane(target, name, receiver) {
-  var apconPrime = apcon.derive(name);
-  return Access(apconPrime);
-});
+  var membrane = Contract.Constructor(function membrane(target, name, receiver) {
+    with(APC.Contract) {
+      var apconPrime = apcon.derive(name); 
+    }
+
+    return any;
+    //var apconPrime = apcon.derive(name);
+    //return  Contract.With(APC.Contract, Access(apconPrime, APC));
+  });
+
+  var g = {};
+//  for(var p in APC.Contract)  {print (p); g[p]=APC.Contract[p]; }
+  g["apcon"]=apcon;
+  g["Access"]=Access;
+  g["APC"]=APC;
+  g["Null"]=APC.Contract.Null;
 
 
- var get = Contract.Get(Contract.And(
-    Contract.AFunction({1:readable}, any),
-    Contract.With({apcon:apcon, Access:Access}, Contract.Dependent(membrane))
-  ));
+  var get = Contract.Get(Contract.And(Contract.AFunction({1:readable}, any),
+        Contract.With(g /*{apcon:apcon,Access:Access,APC:APC,any:any}*/, Contract.Dependent(membrane))));
   //var get = Contract.Get(Contract.AFunction({1:readable}, any));
   var set = Contract.Set(Contract.AFunction({1:writeable}, any));
 
@@ -41,8 +51,3 @@ var membrane = Contract.Constructor(function membrane(target, name, receiver) {
 });
 
 var Access = AccessContract.ctor;
-
-
-
-
-
