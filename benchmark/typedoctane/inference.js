de4aa54d640e08fd73308bd0e0878981f87a6106
keilr <keilr@e3db86c2-31ce-0310-bca5-be3edd1b5589>
2015-01-27 08:35:06 +0000

    sync
    git-svn-id: https://proglang.informatik.uni-freiburg.de/svn/proglang/projects/jsproxy/branches/TreatJS@24964 e3db86c2-31ce-0310-bca5-be3edd1b5589

--- benchmark/typedoctane/inference.js
+++ benchmark/typedoctane/inference.js
@@ -97,9 +97,9 @@ for(var funID in _types_) {
   // function
   _print_("_TYPES_['"+funID+"']=[];");
 
-  for(var callID = 0; callID < _types_[funID].length; callID++) {
+  if(_types_[funID]!==undefined) for(var callID = 0; callID < _types_[funID].length; callID++) {
     _print_("_TYPES_['"+funID+"']['"+callID+"']=[];");
-    for(var argID = -1; argID < _types_[funID][callID].length; argID++) {
+    if(_types_[funID][callID]!==undefined) for(var argID = -1; argID < _types_[funID][callID].length; argID++) {
       _print_("_TYPES_['"+funID+"']['"+callID+"']["+argID+"]='"+_types_[funID][callID][argID]+"';");
     }
   }
