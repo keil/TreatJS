40581d265d7a89f1f88ee6bc621062872d4190b4
keilr <keilr@e3db86c2-31ce-0310-bca5-be3edd1b5589>
2015-02-17 10:06:07 +0000

    new version TreatJS 1.3.1 (PoC)
    git-svn-id: https://proglang.informatik.uni-freiburg.de/svn/proglang/projects/jsproxy/branches/TreatJS@25138 e3db86c2-31ce-0310-bca5-be3edd1b5589

--- src/treat.js
+++ src/treat.js
@@ -26,7 +26,7 @@
 function TreatJS(configuration, verbose, out, dunit) {
   if(!(this instanceof TreatJS)) return new TreatJS(configuration, verbose, out);
 
-  var version = "TreatJS 1.3.0 (PoC)";
+  var version = "TreatJS 1.3.1 (PoC)";
 
   Object.defineProperties(this, {
     "version": { value: version }
