f0ff9b989bf3a2c6a866c4996ff9274f31d9a6f9
keilr <keilr@e3db86c2-31ce-0310-bca5-be3edd1b5589>
2015-02-18 20:16:35 +0000

    napshot
    git-svn-id: https://proglang.informatik.uni-freiburg.de/svn/proglang/projects/jsproxy/branches/TreatJS@25150 e3db86c2-31ce-0310-bca5-be3edd1b5589

--- bootstrap.js
+++ bootstrap.js
@@ -112,8 +112,7 @@ load("src/core/treat.logic.js");
 load("src/core/treat.callback.js");
 
 load("src/core/treat.map.js");
-load("src/core/treat.polymorphism2.js"); // Merge variable to
-// TODO
+load("src/core/treat.polymorphism.js");
 
 load("src/core/treat.contract.js");
 load("src/core/treat.constructor.js");
