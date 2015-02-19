21e35654b93312c80f33010f72dcd6893b372a41
keilr <keilr@e3db86c2-31ce-0310-bca5-be3edd1b5589>
2015-02-19 14:03:44 +0000

    sync
    git-svn-id: https://proglang.informatik.uni-freiburg.de/svn/proglang/projects/jsproxy/branches/TreatJS@25164 e3db86c2-31ce-0310-bca5-be3edd1b5589

--- test/default.js
+++ test/default.js
@@ -20,6 +20,40 @@
 
 // ==================================================
 
+// f : [[Even]->Even] -> Even \cap [[Pos]->Pos] -> Pos
+function f ( g ) {
+  // g : [Even]->Even \cup [Pos]->Pos] 
+  // x : Even \cap Pos
+  g(x); 
+}
+
+
+
+
+
+// plus : [Num,Num] -> Num \cap [Str,Str]-> Str 
+function plus (x, y) {
+  return (x+y);
+}
+
+// tets: [[Num,Num] -> Num] -> Num 
+function test (plus) {
+  return plus(1,2)
+}
+
+
+// add1 : [([Num,Num] -> Num), Num
+function add1(plus, x) {
+  return plus(x, 1);
+}
+
+// inc : 
+function inc (add1) {
+  return add1(plus);
+}
+
+
+
 // ==================================================
 
 TreatJS.Statistic.print();
