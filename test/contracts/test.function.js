7ef8b05d428f4756787fe347641e00c55070ec74
keilr <keilr@e3db86c2-31ce-0310-bca5-be3edd1b5589>
2016-09-13 13:11:00 +0000

    sync
    git-svn-id: https://proglang/svn/proglang/projects/treatjs/branches/TreatJS2@26983 e3db86c2-31ce-0310-bca5-be3edd1b5589

--- test/contracts/test.function.js
+++ test/contracts/test.function.js
@@ -21,7 +21,7 @@ new Test('Contract/Function/1', function() {
   this.expect(function() {
     var plusNumber = Contract.assert(plus, Contract.Function(Contract.Object([typeNumber, typeNumber]),typeNumber));
     plusNumber(1,"1");
-  }).subjectBlame();
+  }).contextBlame();
 
   this.expect(function() {
     var plusNumber = Contract.assert(plus, Contract.Function(Contract.Object([typeNumber, typeNumber]),typeNumber));
@@ -30,10 +30,49 @@ new Test('Contract/Function/1', function() {
 
   this.expect(function() {
     var plusNumber = Contract.assert(faultyPlus, Contract.Function(Contract.Object([typeNumber, typeNumber]),typeNumber));
-    plusNumber("1",1);
+    plusNumber(1,1);
   }).subjectBlame();
 
 });
 
-// addone
-//
+new Test('Contract/Function/2', function() {
+
+  function plus(x, y) {
+    return x+y;
+  }
+
+  function faultyPlus(x, y) {
+    return ""+x+y;
+  }
+
+  function addOne(plus, z) {
+    return plus(z, 1);
+  }
+
+  function faultyAddOne(plus, z) {
+    return plus(z, "1");
+  }
+
+  var NumNum_Num = Contract.Function(Contract.Object([typeNumber, typeNumber]),typeNumber);
+
+  this.expect(function() {
+    var addOneNumber = Contract.assert(addOne, Contract.Function(Contract.Object([NumNum_Num, typeNumber]),typeNumber));
+    addOneNumber(plus, 1);
+  }).noBlame();
+
+  this.expect(function() {
+    var addOneNumber = Contract.assert(addOne, Contract.Function(Contract.Object([NumNum_Num, typeNumber]),typeNumber));
+    addOneNumber(plus, "1");
+  }).contextBlame();
+
+  this.expect(function() {
+    var addOneNumber = Contract.assert(addOne, Contract.Function(Contract.Object([NumNum_Num, typeNumber]),typeNumber));
+    addOneNumber(faultyPlus, 1);
+  }).contextBlame();
+
+  this.expect(function() {
+    var addOneNumber = Contract.assert(faultyAddOne, Contract.Function(Contract.Object([NumNum_Num, typeNumber]),typeNumber));
+    addOneNumber(plus, 1);
+  }).subjectBlame();
+
+});
