138182dd110e8e6cd0f22d26189397708b5e28b9
keilr <keilr@e3db86c2-31ce-0310-bca5-be3edd1b5589>
2017-03-04 12:55:36 +0000

    sync
    git-svn-id: https://proglang/svn/proglang/projects/treatjs/branches/TreatJS2@27151 e3db86c2-31ce-0310-bca5-be3edd1b5589

---
+++ tests/semantics/test.picky.js
@@ -0,0 +1,81 @@
+new Test('Semantics/Picky/1', function() {
+
+  let plus = Contract.assert(function plus(x, y) {
+    return x+y;
+  }, Contract.Function([typeNumber, typeNumber], typeNumber));
+
+  let plus2 = Contract.assert(function plus(x, y) {
+    return ""+x+y;
+  }, Contract.Function([typeNumber, typeNumber], typeNumber));
+
+  let test = Contract.Base(function(f) {
+    return f("1",2);
+  });
+
+  let test2 = Contract.Base(function(f) {
+    return f(1,2);
+  });
+
+  this.expect(function() {
+    Contract.assert(plus, test2);
+  }).noBlame();
+
+  this.expect(function() {
+    Contract.assert(plus, test);
+  }).contextBlame();
+
+  this.expect(function() {
+    Contract.assert(plus2, test2);
+  }).subjectBlame();
+
+});
+
+new Test('Semantics/Picky/2', function() {
+
+  let id = Contract.assert(function id(x) {
+    return x;
+  }, Contract.Intersection.from(
+    Contract.Function([Contract.Function([Positive], Positive)], Contract.Base(f => f(1))),
+    Contract.Function([Contract.Function([Even], Even)], Contract.Base(f => f(-2)))
+    )
+  );
+
+  this.expect(function() {
+    id(x => x)(1)
+  }).subjectBlame();
+
+  this.expect(function() {
+    id(x => x)(-1)
+  }).subjectBlame();
+
+  this.expect(function() {
+    id(x => x)(2)
+  }).noBlame();
+
+});
+
+new Test('Semantics/Picky/3', function() {
+
+  let id = Contract.assert(function id(x) {
+    return x;
+  }, Contract.Function([Natural], Positive));
+
+  let idTest = (Contract.Constructor(function test(id) {
+    return Contract.Base(function test(subject) {
+      return subject == id(subject);
+    }, "ID Test");
+  }))(id);
+
+  this.expect(function() {
+    Contract.assert(1, idTest);
+  }).noBlame();
+
+  this.expect(function() {
+    Contract.assert("a", idTest);    
+  }).contextBlame();
+
+  this.expect(function() {
+    Contract.assert(0, idTest);    
+  }).subjectBlame();
+
+});
